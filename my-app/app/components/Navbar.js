39450278ed5e4f13b0d44bf6b60725b8b52158ff
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-01-31 22:32:12 +0530

    Some changes made

--- my-app/app/components/Navbar.js
+++ my-app/app/components/Navbar.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      <div>Yukti</div>
+    <div className='m-2 p-2 flex gap-10'>
+      <div className='mx-auto'>Yukti</div>
+      <ul className='flex gap-5'>
+        <li className='hover:text-red-600 cursor-pointer'>Home</li>
+        <li className='hover:text-red-600 cursor-pointer'>About Us</li>
+        <li className='hover:text-red-600 cursor-pointer'>Contact Us</li>
+      </ul>
     </div>
   )
 }
