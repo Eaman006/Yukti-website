33cf6fc2006143c6c7c3095db5c348944d246b2a
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-02-07 21:13:45 +0530

    logo changes are made

--- my-app/app/components/Navbar.js
+++ my-app/app/components/Navbar.js
@@ -13,17 +13,18 @@ const myFont = localFont({ src: './Montserrat-VariableFont_wght.ttf' });
 const Navbar = () => {
   const { theme, toggleTheme } = useTheme();
   const pathname = usePathname();
-  const getpathname=(path)=>{
-    return pathname===path ? 'text-red-600':'hover:text-red-600';
+  const getpathname = (path) => {
+    return pathname === path ? 'text-red-600' : 'hover:text-red-600';
   };
-  
+
 
   return (
     <div className={`m-2 p-2 flex justify-between items-center bg-white dark:bg-gray-900 text-black dark:text-white shadow-md ${myFont.className}`}>
-      <div className="text-4xl font-bold">Yukti
-      <div className="text-xs font-normal">Experience, Engage, Excel</div>       
+      <div className="">
+        <div className="text-4xl font-bold text-center">Yukti</div>
+        <div className="text-xs font-normal text-center">Experience, Engage, Excel</div>
       </div>
-      
+
 
       <ul className="flex gap-5 font-bold">
         <li className={`cursor-pointer p-2 ${getpathname('/')}`}>
@@ -35,15 +36,15 @@ const Navbar = () => {
         <li className={`cursor-pointer p-2 ${getpathname('/Contact')}`}>
           <Link href="/Contact">Contact Us</Link></li>
         <button
-        onClick={toggleTheme}
-        className="p-2 rounded-full transition-all duration-300 bg-gray-200 dark:bg-gray-800"
-      >
-        {theme === "dark" ? (
-          <SunIcon className="w-6 h-6 text-yellow-400" />
-        ) : (
-          <MoonIcon className="w-6 h-6 text-gray-800" />
-        )}
-      </button>
+          onClick={toggleTheme}
+          className="p-2 rounded-full transition-all duration-300 bg-gray-200 dark:bg-gray-800"
+        >
+          {theme === "dark" ? (
+            <SunIcon className="w-6 h-6 text-yellow-400" />
+          ) : (
+            <MoonIcon className="w-6 h-6 text-gray-800" />
+          )}
+        </button>
       </ul>
 
     </div>
