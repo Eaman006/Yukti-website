12251912d327b8d37782f4a1cbf388ecfa52351e
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-02-07 21:31:45 +0530

    mission added

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,6 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      <div className="m-2 py-5 text-center px-20 font-bold">
+        At Yukti, we believe every individual deserves a fun, safe space to learn and grow. Our mission is to empower the neurodiverse by creating engaging virtual worlds where everyday skills are built, right from home.
+      </div>
 
     </div>
   );
