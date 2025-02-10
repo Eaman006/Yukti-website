0323a3c292bfe9751d19a51e69f68422224588c1
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-02-10 22:36:31 +0530

    some changes

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="bg-[#1A73E8] w-full h-screen">
         
       </div>
-      <div className="m-2 py-5 text-center px-20 font-bold">
+      <div className="py-5 text-center px-20 font-bold ">
         At Yukti, we believe every individual deserves a fun, safe space to learn and grow. Our mission is to empower the neurodiverse by creating engaging virtual worlds where everyday skills are built, right from home.
       </div>
 
