37353cceab14532d84592e6bbcc3a92cd225d011
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-02-15 14:25:44 +0530

    commit some changes

--- my-app/app/components/Navbar.js
+++ my-app/app/components/Navbar.js
@@ -29,11 +29,7 @@ const Navbar = () => {
       {/* Loader with Blur Effect */}
       {isPending && (
         <div className="fixed inset-0 flex items-center justify-center bg-white/30 dark:bg-gray-900/30 backdrop-blur-md z-40">
-          <div className="relative w-16 h-16">
-  <div className="absolute inset-0 border-4 border-transparent border-t-blue-500 border-l-blue-500 rounded-full animate-spin"></div>
-  <div className="absolute inset-2 border-4 border-transparent border-b-blue-500 border-r-blue-500 rounded-full animate-[spin_1.5s_linear_infinite]"></div>
-</div>
-
+          <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
         </div>
       )}
 
